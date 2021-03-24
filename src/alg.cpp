684658c7452942ff27f636e3285a57d106801dda
KhmelnikovRoman <78897772+KhmelnikovRoman@users.noreply.github.com>
2021-03-24 23:51:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,28 +8,20 @@ int cbinsearch(int *arr, int size, int value) {
     while ((r - l) > 0) {
         if (arr[r - 1] == value && r == size) {
             while (arr[r - 1 - l] == value) {
-                l += 1;
-            }
-            return (l);
-        }
+                l += 1;}
+            return (l);}
         else if (arr[(r - l) / 2] < value) {
-            l = (r - l) / 2;
-        }
+            l = (r - l) / 2;}
         else if (arr[(r - l) / 2] == value) {
             a = (r - l) / 2;
             r = 0;
             l = 0;
             while (arr[a + r] == value) {
-                r += 1;
-            }
+                r += 1;}
             while (arr[a - l] == value) {
-                l += 1;
-            }
-            return (r + l - 1);
-        }
+                l += 1;}
+            return (r + l - 1);}
         else if (arr[(r - l) / 2] > value) {
-            r = (r - l) / 2;
-        }
+            r = (r - l) / 2;}
     }
-    return 0;
-}
+    return 0;}
